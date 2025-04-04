57d72c5a43d8193132592c38d2f16e5d67d01a48
Sorin Marian <marian.sorin@yahoo.com>
2025-04-04 11:08:43 +0300

    Rename docker service name

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ echo 'Building containers...'
 docker-compose up -d --build
 
 echo 'Installing composer...'
-docker exec -it bands-service-php composer install --prefer-dist --no-progress --no-interaction
+docker exec -it bands-service-app composer install --prefer-dist --no-progress --no-interaction
 
 echo 'Creating database...'
-docker exec -it bands-service-php php bin/console doctrine:database:create --if-not-exists --no-interaction
+docker exec -it bands-service-app php bin/console doctrine:database:create --if-not-exists --no-interaction
 
 echo 'Running migrations...'
-docker exec -it bands-service-php php bin/console doctrine:migrations:migrate --no-interaction
-docker exec -it bands-service-php php bin/console cache:clear --no-interaction
+docker exec -it bands-service-app php bin/console doctrine:migrations:migrate --no-interaction
+docker exec -it bands-service-app php bin/console cache:clear --no-interaction
 
 echo 'Generate swagger documentation...'
-docker exec -it bands-service-php php bin/console api:openapi:export --format=json > swagger.json
+docker exec -it bands-service-app php bin/console api:openapi:export --format=json > swagger.json
